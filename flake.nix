b91734bd0c44f0eb8baa0fd369bdb68facf4ae6d
lcwllmr <159539641+lcwllmr@users.noreply.github.com>
2025-05-26 21:45:36 +0200

    add flake providing this as a package

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in {
+          default = pkgs.writeShellScriptBin "pyjlab" (builtins.readFile ./pyjlab);
+        });
+    };
+}
